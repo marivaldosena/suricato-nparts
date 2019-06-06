3082d6b29a1e9d131df7968dd85fca4c5f2df0b0
Adenilton Henrique Ribeiro <adhenriquemusic@gmail.com>
2019-06-06 19:01:47 -0300

    iniciando endpoit customer

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -2,16 +2,14 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Resources\UserResource;
-use App\User;
-use Validator;
+use App\Customer;
+use App\Http\Resources\CustomerResource;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Http\Resources\Json\ResourceCollection;
-use Illuminate\Support\Facades\DB;
 
-class UserController extends Controller
+class CustomerController extends Controller
 {
     private $rules = [
         'name' => 'required|min:3',
@@ -21,17 +19,17 @@ class UserController extends Controller
     ];
 
     /**
-     * Display a listing of the Users.
+     * Display a listing of the Customer.
      *
      * @return ResourceCollection
      */
     public function index()
     {
-        return UserResource::collection(User::paginate(10));
+        return CustomerResource::collection(Customer::paginate(10));
     }
 
     /**
-     * Store a newly created User in storage.
+     * Store a newly created Customer in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
@@ -40,38 +38,21 @@ class UserController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, $this->rules);
-
-        $exists = User::where('email', $request->email)->first();
-
-        if ($exists) {
-            return response()->json(['message' => 'This email is already registered.'], 409);
-        }
-
-        // TODO - Enviar email de confirmação
-        $user = new User;
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = bcrypt($request->password);
-        $user->type = $request->type;
-
-        $user->save();
-
-        return response(null, 201);
     }
 
     /**
-     * Display the specified resource.
+     * Display the specified Customer.
      *
      * @param  int  $id
      * @return JsonResource
      */
     public function show($id)
     {
-        return new UserResource(User::findOrFail($id));
+        return new CustomerResource(Customer::findOrFail($id));
     }
 
     /**
-     * Update the specified User in storage.
+     * Update the specified Customer in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
@@ -81,30 +62,19 @@ class UserController extends Controller
     public function update(Request $request, $id)
     {
         $this->validate($request, $this->rules);
-
-        $user = User::findOrFail($id);
-
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = bcrypt($request->password);
-        $user->type = $request->type;
-
-        $user->save();
-
-        return response(null, 204);
     }
 
     /**
-     * Remove the specified User from storage.
+     * Remove the specified Customer from storage.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        $user = User::findOrFail($id);
+        $customer = Customer::findOrFail($id);
 
-        $user->delete();
+        $customer->delete();
 
         return response(null, 204);
     }
