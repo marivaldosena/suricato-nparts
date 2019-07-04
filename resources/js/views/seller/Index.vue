119d3b5d2a54b21459a6ab45e0c66c25a1825689
Adenilton Henrique Ribeiro <adhenriquemusic@gmail.com>
2019-07-03 22:06:01 -0300

    adicionando alertas nas paginas de buyer e seller

--- resources/js/views/seller/Index.vue
+++ resources/js/views/seller/Index.vue
@@ -1,17 +1,20 @@
 <template>
     <div>
         <Title/>
+        <Alert/>
         <p>Área do usuário vendedor</p>
     </div>
 </template>
 
 <script>
     import Title from './../../components/Title';
+    import Alert from './../../components/Alert/Alert';
 
     export default {
         name: "Index",
         components: {
             Title,
+            Alert,
         },
     }
 </script>
